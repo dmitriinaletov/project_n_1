399d2e3f7236780b4800f4890ab7d7d04d7f98e5
Dmitrii Naletov <dmitriinaletov@gmail.com>
2022-10-09 14:37:49 +0200

    The_third_commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './Header.css';
+import s from './Header.module.css';
 
 const Header = () => {
-    return <header className='header'>
+    return <header className = {s.header}>
         <img src='https://e7.pngegg.com/pngimages/140/849/png-clipart-jumpman-air-jordan-nike-converse-sneakers-nike-logo-basketballschuh-thumbnail.png' />
     </header>;
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
-import classes from './Navbar.module.css';
+import s from './Navbar.module.css';
+console.log(s);
+
+// let s = {
+//    'nav': 'Navbar_nav__wXPlc',
+//    'item': 'Navbar_item__DpmYn'
+//    'active': 'Navbar_active__something'
+// }
+
+let c1 = 'item';
+let c2 = 'active';
+// 'item active'
+let classes = c1 + ' ' + c2;
+let classesNew = `${s.item} ${c2}`;
 
 const Navbar = () => {
-   return <nav className='Navbar_nav__wXPlc'>
-      <div className = 'Navbar_item__DpmYn'>
+   return <nav className = {s.nav}>
+      <div className = {s.item}>
          <a>Profile</a>
       </div>
-      <div className = 'Navbar_item__DpmYn'>
+      <div className = {`${s.item} ${s.active}`}>
          <a>Masseges</a>
       </div>
-      <div className = 'Navbar_item__DpmYn'>
+      <div className = {s.item}>
          <a>News</a>
       </div>
-      <div className = 'Navbar_item__DpmYn'>
+      <div className = {s.item}>
          <a>Music</a>
       </div>
-      <div className = 'Navbar_item__DpmYn'>
+      <div className = {s.item}>
          <a>Settings</a>
       </div>
    </nav>;
