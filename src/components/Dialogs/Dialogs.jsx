043018cb6b7d78dcf9a53a3ae291038f3223bf92
Dmitrii Naletov <dmitriinaletov@gmail.com>
2022-10-24 22:41:28 +0200

    Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,9 +3,33 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
    return (
-      <div>
-         Dialogs
-      </div>
+      <div className={s.dialogs}>
+         <div className={s.dialogsItems}>
+            <div className={s.dialog + ' ' + s.active}>
+               Dima
+            </div>
+            <div className={s.dialog}>
+               Khrystyna
+            </div>
+            <div className={s.dialog}>
+               Andrei
+            </div>
+            <div className={s.dialog}>
+               Martin
+            </div>
+            <div className={s.dialog}>
+               Valera
+            </div>
+            <div className={s.dialog}>
+               Honza
+            </div>
+         </div>
+         <div className={s.messeges}>
+            <div className={s.messege}>Hi</div>
+            <div className={s.messege}>How are you?</div>
+            <div className={s.messege}>I am fine</div>
+         </div>
+      </div >
    )
 }
 
