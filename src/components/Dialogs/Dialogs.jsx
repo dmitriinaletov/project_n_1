5219d4d33eb087ab6c1da06f8c1b729d2910fc60
Dmitrii Naletov <dmitriinaletov@gmail.com>
2022-10-25 19:59:29 +0200

    Route_exact

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,27 +1,28 @@
 import React from "react"
 import s from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Dialogs = (props) => {
    return (
       <div className={s.dialogs}>
          <div className={s.dialogsItems}>
             <div className={s.dialog + ' ' + s.active}>
-               Dima
+               <NavLink to='/dialogs/1' className={navData => navData.isActive ? s.active : s.item}>Dima</NavLink>
             </div>
             <div className={s.dialog}>
-               Khrystyna
+               <NavLink to='/dialogs/2' className={navData => navData.isActive ? s.active : s.item}>Khrystyna</NavLink>
             </div>
             <div className={s.dialog}>
-               Andrei
+               <NavLink to='/dialogs/3' className={navData => navData.isActive ? s.active : s.item}>Andrei</NavLink>
             </div>
             <div className={s.dialog}>
-               Martin
+               <NavLink to='/dialogs/4' className={navData => navData.isActive ? s.active : s.item}>Martin</NavLink>
             </div>
             <div className={s.dialog}>
-               Valera
+               <NavLink to='/dialogs/5' className={navData => navData.isActive ? s.active : s.item}>Valera</NavLink>
             </div>
             <div className={s.dialog}>
-               Honza
+               <NavLink to='/dialogs/6' className={navData => navData.isActive ? s.active : s.item}>Honza</NavLink>
             </div>
          </div>
          <div className={s.messeges}>
