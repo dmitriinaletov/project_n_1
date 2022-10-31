0ee6a00580defd2e72fcf6b99f5b7e335878916f
dmitriinaletov <dmitriinaletov@gmail.com>
2022-10-31 16:14:59 +0100

    Map_Array

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,10 +4,12 @@ import Post from './Post/Post';
 
 const MyPosts = (props) => {
 
-    let postsData = [
+    let posts = [
         { id: 1, message: 'Hi, how are you?', likesCount: 12 },
         { id: 2, message: "It's my first post", likesCount: 23 },
     ]
+
+    let postsElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount} />)
 
     return (
         <div className={s.postsBlock}>
@@ -21,8 +23,7 @@ const MyPosts = (props) => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message={postsData[0].message} likesCount={postsData[0].likesCount} />
-                <Post message={postsData[1].message} likesCount={postsData[1].likesCount} />
+                {postsElements}
             </div>
         </div>)
 }
